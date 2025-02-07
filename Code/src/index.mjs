b5cfe284ceed18c453c4e8a6ce9ec20a38eb3e4c
Paras Chandra <paraschandra30@gmail.com>
2025-02-07 16:04:08 +0530

    cookies

--- Code/src/index.mjs
+++ Code/src/index.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from './routes/index.mjs';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json()); // middleware
+app.use(cookieParser("helloworld"));
 app.use(routes);
 
 // for global use of middleware
@@ -12,6 +14,7 @@ app.use(routes);
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+    res.cookie('hello', 'world', {maxAge: 10000, signed: true});
     res.status(201).send({msg: 'Hello, Express!'});
 });
 
--- Code/src/routes/products.mjs
+++ Code/src/routes/products.mjs
@@ -3,11 +3,16 @@ import { Router } from 'express';
 const router = Router();
 
 router.get("/api/products", (req, res) => {
-    res.send([
-        {id: 1, product: 'soap'},
-        {id: 2, product: 'milk'},
-        {id: 3, product: 'perfume'},
-    ]);
+    console.log(req.headers.cookie);
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+    if(req.signedCookies.hello && req.signedCookies.hello === 'world')
+        res.send([
+            {id: 1, product: 'soap'},
+            {id: 2, product: 'milk'},
+            {id: 3, product: 'perfume'},
+        ]);
+    return res.send({msg: "Sorry, you need the right cookie."});
 });
 
 
