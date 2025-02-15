19b38089f0f54fe085464b392a13c49a38beeb33
Paras Chandra <paraschandra30@gmail.com>
2025-02-15 12:55:05 +0530

    passport js authentication

--- Code/src/index.mjs
+++ Code/src/index.mjs
@@ -2,7 +2,9 @@ import express from "express";
 import routes from './routes/index.mjs';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import passport from 'passport';
 
+import './strategies/local-strategy.mjs';
 import { users } from "./utils/constants.mjs";
 
 const app = express();
@@ -17,6 +19,9 @@ app.use(session({
         maxAge: 60000 * 60,
     },
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
 
 // for global use of middleware
@@ -36,23 +41,45 @@ app.get("/", (req, res) => {
     res.status(201).send({msg: 'Hello, Express!'});
 });
 
-app.post('/api/auth', (req, res) => {
-    const { body: {username, password} } = req;
-    const findUser = users.find((user) => user.username === username);
-    if(!findUser || findUser.password !== password) 
-        return res.status(404).send({msg: "BAD CREDENTIALS"});
+// app.post('/api/auth', (req, res) => {
+//     const { body: {username, password} } = req;
+//     const findUser = users.find((user) => user.username === username);
+//     if(!findUser || findUser.password !== password) 
+//         return res.status(404).send({msg: "BAD CREDENTIALS"});
 
-    req.session.user = findUser;
-    return res.status(200).send(findUser);
-});
+//     req.session.user = findUser;
+//     return res.status(200).send(findUser);
+// });
+
+app.post('/api/auth', 
+    passport.authenticate("local"), 
+    (req, res) => {
+        res.sendStatus(200);
+    }
+);
+
+// app.get('/api/auth/status', (req, res) => {
+//     req.sessionStore.get(req.sessionID, (err, session) => {
+//         console.log(session);
+//     });
+//     return req.session.user ? 
+//         res.status(200).send(req.session.user) :
+//         res.status(401).send({msg: "Not Authenticated"});
+// });
 
 app.get('/api/auth/status', (req, res) => {
-    req.sessionStore.get(req.sessionID, (err, session) => {
-        console.log(session);
+    console.log(`Inside /auth/status endpoint`);
+    console.log(req.user);
+    console.log(req.session);
+    return req.user ? res.send(req.user) : res.sendStatus(401);
+});
+
+app.post('/api/auth/logout', (req, res) => {
+    if(!req.user) return res.sendStatus(401);
+    req.logout((err) => {
+        if(err) return res.sendStatus(400);
+        res.sendStatus(200);
     });
-    return req.session.user ? 
-        res.status(200).send(req.session.user) :
-        res.status(401).send({msg: "Not Authenticated"});
 });
 
 app.post('/api/cart', (req, res) => {
---
+++ Code/src/strategies/local-strategy.mjs
@@ -0,0 +1,38 @@
+import passport from 'passport';
+import { Strategy } from 'passport-local';
+import { users } from '../utils/constants.mjs';
+
+passport.serializeUser((user, done) => {
+    console.log(`Inside serialize user`);
+    console.log(user);
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    console.log(`Inside deserializer`);
+    console.log(`Deserializing User ID: ${id}`);
+    try {
+        const findUser = users.find((user) => user.id === id);
+        if(!findUser) throw new Error("User not found");
+        done(null, findUser);
+    } catch (err) {
+        done(err, null);
+    }
+});
+
+export default passport.use(
+    new Strategy((username, password, done) => {
+        console.log(`Username: ${username}`);
+        console.log(`Password: ${password}`);
+        try {
+            const findUser = users.find((user) => user.username === username);
+            if(!findUser) throw new Error("User not found");
+            if(findUser.password !== password)
+                throw new Error("Invalid Credentials");
+
+            done(null, findUser);
+        } catch (err) {
+            done(err, null);
+        }
+    })
+)
